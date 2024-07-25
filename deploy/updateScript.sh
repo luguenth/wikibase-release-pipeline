5ddab5e8a466ca469e2da2c166713024faace8aa
Kolja Bailly <kolja.bailly@tib.eu>
2024-07-25 13:07:15 +0200

    ongoing work

--- deploy/updateScript.sh
+++ deploy/updateScript.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+su nobody
+
+composer require mediawiki/semantic-media-wiki
+
 # Install Composer dependencies
-composer install
+#composer install
 
 # Update Composer dependencies
-composer update
+#composer update
 
 # Keeping the container running
 tail -f /dev/null
