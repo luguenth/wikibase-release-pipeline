fd066144010da06de4aa1702adb284d47b441941
Lukas Günther <lukas.guenther@tib.eu>
2024-07-26 14:20:16 +0200

    working plugin installation

--- deploy/updateScript.sh
+++ deploy/updateScript.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
-composer require mediawiki/semantic-media-wiki:* --ignore-platform-req=php
-composer update --no-dev
+chown -R root:root .
 
-echo "done" 
+# check if path semanti media wiki is already in ectensions
+if [ ! -d "extensions/SemanticMediaWiki" ]; then
+    composer require mediawiki/semantic-media-wiki:* --ignore-platform-req=php
+fi
+
+# run mediawiki update script 
+php maintenance/update.php --quick
+#composer update --no-dev
+
+#echo "done" 
 # Install Composer dependencies
 #composer install
 
 # Update Composer dependencies
 #composer update
 
-cp /opt/LocalSettingsExtra.d/* LocalSettings.d/
+cp /opt/mw/LocalSettingsExtra.d/* LocalSettings.d/
 
 # Keeping the container running
 tail -f /dev/null
